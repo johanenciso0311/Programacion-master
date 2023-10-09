464cb02c4492339d2c48b83b35b553ab50c76639
steven2.0 <encisoholguinsteven@gmial.com>
2023-10-09 17:45:22 +0200

    111

---
+++ src/PrimeraEvaluacion/Tema02/ejemplos/variables/Ejercicio24.java
@@ -0,0 +1,11 @@
+package PrimeraEvaluacion.Tema02.ejemplos.variables;
+
+import java.util.Scanner;
+
+public class Ejercicio24 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+
+    }
+}
